ec2de10ef7f595a13b356469ca172d37d4010af9
AntonAlbertovich <anton.a.tech@gmail.com>
2018-10-10 15:44:22 -0500

    new file:   chess_main.py 	renamed:    reading_rainbow.py -> write_new_mega_chess_bin.py

---
+++ chess_main.py
@@ -0,0 +1,13 @@
+import chess_bin_functions
+import pickle
+
+
+
+
+chess_bin_functions.read_file_sizes()
+
+chess_bin_functions.read_file_total_moves()
+
+chess_bin_functions.print_a_game( 0, 0)
+
+
